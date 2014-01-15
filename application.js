390b44259fe33805ac8c038c17edeb629e2c7258
yeefy <yftran@gmail.com>
2014-01-14 19:33:55 -0800

    Complete

--- application.js
+++ application.js
@@ -11,24 +11,38 @@ $(document).ready(function() {
 	
 	
 	$("#submit").click(function() {
-			
+		
 			var guessNumber = $("#guess").val();
+			
+			if (guessNumber<=0 || guessNumber >=101){alert("Choose a number between 1 and 100!")};
+			
+			if (isNaN(guessNumber)) {alert("That's not a number!")};
+		
 			var difference = Math.abs(guessNumber-randomNumber);
-		
-		
+			var printNumber = $("#number").text(guessNumber);
+			
 			if(difference==0){
-				$("#answerbox").text("Correct!");
-				$("#number").text(guessNumber)
+				$("#answerbox").text("Correct! Play Again!");
+				printNumber
+				$("#guess").val("");	
+				randomNumber = Math.floor((Math.random()*100)+1) ;
+			}else if(difference<5){
+				$("#answerbox").text("Hot!");
+				printNumber
 				$("#guess").val("");
 			}else if(difference<10){
-				$("#answerbox").text("Hot!");
-				$("#number").text(guessNumber)
+				$("#answerbox").text("Warmer!");
+				$("#guess").val("");
+			}else if (difference<15){
+				$("#answerbox").text("Cold!");
 				$("#guess").val("");
 			}else{
-				$("#answerbox").text("Cold!");
-				$("#number").text(guessNumber);
+				$("#answerbox").text("Freezing!");
+				printNumber
 				$("#guess").val("");
+				
 		};
+		
 		
 				
 	});
